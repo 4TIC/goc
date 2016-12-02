99d2ebe2c405c46dcf589a8083cc745c04b37035
Ricardo Borillo <borillo@gmail.com>
2016-12-02 14:14:59 +0000

    Cambios modelo de datos para multidioma
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@32732 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/resources/database/goc_reuniones_0.1.0.sql
+++ goc-base/src/main/resources/database/goc_reuniones_0.1.0.sql
@@ -1,3 +1,22 @@
 alter table goc_reuniones modify (duracion null);
 
 alter table goc_reuniones add (fecha_segunda_convocatoria date null);
+
+alter table goc_cargos add (nombre_alt varchar2(400));
+alter table goc_organos add (nombre_alt varchar2(400));
+alter table goc_organos_reuniones modify (organo_nombre varchar2(400));
+alter table goc_organos_reuniones add (organo_nombre_alt varchar2(400));
+alter table goc_organos_reuniones_miembros add (cargo_nombre_alt varchar2(400));
+alter table goc_p_orden_dia_documentos add (descripcion_alt varchar2(2048));
+alter table goc_reuniones add (asunto_alt varchar2(500));
+alter table goc_reuniones add (descripcion_alt clob);
+alter table goc_reuniones add (acuerdos_alt clob);
+alter table goc_reuniones add (ubicacion_alt varchar2(4000));
+alter table goc_reuniones add (telematica_descripcion_alt clob);
+alter table goc_reuniones_documentos add (descripcion_alt varchar2(2048));
+alter table goc_reuniones_puntos_orden_dia add (titulo_alt varchar2(4000));
+alter table goc_reuniones_puntos_orden_dia add (descripcion_alt clob);
+alter table goc_reuniones_puntos_orden_dia add (acuerdos_alt clob);
+alter table goc_reuniones_puntos_orden_dia add (deliberaciones_alt clob);
+alter table goc_tipos_organo modify (nombre varchar2(400));
+alter table goc_tipos_organo add (nombre_alt varchar2(400));
