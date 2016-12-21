4c67e9207229238d516e4675ad8203fd8e2a4d1d
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2016-12-21 09:36:41 +0000

    GOC-211 El nombre de la pestaña "Tipos de órganos" aparece siempre en valenciano, también las columnas del grid
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@32923 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/webapp/app/view/tipoOrgano/Main.js
+++ goc-base/src/main/webapp/app/view/tipoOrgano/Main.js
@@ -1,7 +1,7 @@
 Ext.define('goc.view.tipoOrgano.Main', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.tipoOrganoMain',
-    title: 'Tipus d\'Òrgans',
+    title: appI18N ? appI18N.tipoOrganos.titulo : 'Tipus d\'Òrgans',
     requires : [ 'goc.view.tipoOrgano.Grid' ],
     layout: 'fit',
 
