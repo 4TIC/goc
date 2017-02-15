36bc8138d4dcc1e9a8a41eac3a6056e22e7a2dda
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2017-02-15 14:57:32 +0000

    GOC-237 En el grid de reuniones aparece la duración siempre en valenciano
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@33633 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/webapp/app/view/reunion/ReunionGrid.js
+++ goc-base/src/main/webapp/app/view/reunion/ReunionGrid.js
@@ -41,11 +41,11 @@ reunionesGridColumns.push({
     renderer: function (value) {
         var duracionTexto =
         {
-            30: '0,5 hores',
-            60: '1 hora',
-            90: '1,5 hores',
-            120: '2 hores',
-            150: '2,5 hores'
+            30: '0,5 ' + appI18N.reuniones.horas,
+            60: '1 ' + appI18N.reuniones.hora,
+            90: '1,5 ' + appI18N.reuniones.horas,
+            120: '2 ' + appI18N.reuniones.horas,
+            150: '2,5 ' + appI18N.reuniones.horas
         };
 
         return duracionTexto[value];
