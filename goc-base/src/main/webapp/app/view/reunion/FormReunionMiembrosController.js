39c20b0aee857d41f4c1dbee532c15b8a63a289b
Nicolás Manero Carbo <nicolas.manero@4tic.com>
2017-02-15 22:53:37 +0000

    GOC-240 En una reunión al intentar abrir la ventana para asignar sustitutos falla el javascript
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@33638 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/webapp/app/view/reunion/FormReunionMiembrosController.js
+++ goc-base/src/main/webapp/app/view/reunion/FormReunionMiembrosController.js
@@ -2,7 +2,7 @@ Ext.define('goc.view.reunion.FormReunionMiembrosController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.formReunionMiembrosController',
     onClose: function () {
-        var win = this.getView().up('window'); 
+        var win = this.getView(); 
         if (win) {
             win.destroy();
         }
