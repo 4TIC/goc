ccb9c9a0ed7dfc2c01366db5c96fe2adfda23e79
Miguel Ortiz <miguel@4tic.com>
2017-03-27 13:28:16 +0000

    GOC-271     Al filtrar por descriptor/clave en la lista de publicación de acuerdos da error
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@34166 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-core/src/main/java/es/uji/apps/goc/dao/ReunionDAO.java
+++ goc-core/src/main/java/es/uji/apps/goc/dao/ReunionDAO.java
@@ -311,7 +311,6 @@ public class ReunionDAO extends BaseDAODatabaseImpl
                 .and(qClave.id.eq(claveId))
                 .and(beWhere))
             .orderBy(qReunion.fechaCreacion.desc())
-            .distinct()
             .list(qReunion);
     }
 }
