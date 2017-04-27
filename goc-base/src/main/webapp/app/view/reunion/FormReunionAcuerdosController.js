809198e2fe958a162c4ac0368a58a4fd1dae04d4
Francisco Meliá <fmelia@gmail.com>
2017-04-27 11:23:12 +0000

    GOC-294 No se actualiza correctamente el histórico de reuniones
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@34632 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/webapp/app/view/reunion/FormReunionAcuerdosController.js
+++ goc-base/src/main/webapp/app/view/reunion/FormReunionAcuerdosController.js
@@ -5,16 +5,24 @@ Ext.define('goc.view.reunion.FormReunionAcuerdosController',
     onClose : function()
     {
         var win = Ext.WindowManager.getActive();
+
         var grid = Ext.ComponentQuery.query('grid[name=reunion]')[0];
         grid.getSelectionModel().deselectAll();
         grid.getStore().reload();
+
+        var gridHistorico = Ext.ComponentQuery.query('historicoReunionGrid')[0];
+        if (gridHistorico)
+        {
+            gridHistorico.getStore().reload();
+        }
 
         if (win)
         {
             win.destroy();
         }
 
-        grid.up('panel').down('grid[name=ordenDia]').clearStore();
+        var gridOrdenDia = grid.up('panel').down('grid[name=ordenDia]');
+        if (gridOrdenDia) gridOrdenDia.clearStore();
     },
 
     onSaveRecord : function(button, context)
@@ -28,20 +36,22 @@ Ext.define('goc.view.reunion.FormReunionAcuerdosController',
             {
                 url : '/goc/rest/reuniones/' + data.id + '/completada',
                 method : 'PUT',
-                jsonData: data,
+                jsonData : data,
                 success : function(response)
                 {
                     this.onClose();
                 },
-                scope: this
+                scope : this
             });
         }
     },
 
-    afterRenderFormCerrarActa: function(windowFormCerrarActa) {
+    afterRenderFormCerrarActa : function(windowFormCerrarActa)
+    {
         var height = Ext.getBody().getViewSize().height;
-        if (windowFormCerrarActa.getHeight() > height) {
-            windowFormCerrarActa.setHeight(height-30);
+        if (windowFormCerrarActa.getHeight() > height)
+        {
+            windowFormCerrarActa.setHeight(height - 30);
             windowFormCerrarActa.setPosition(windowFormCerrarActa.x, 15);
         }
     }
