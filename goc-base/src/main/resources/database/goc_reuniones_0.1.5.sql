650a7506de9f293b2ae5e54d75bdb6dc32e7c082
Francisco Meliá <fmelia@gmail.com>
2017-06-12 08:56:43 +0000

    GOC-335 Los secretarios deben poder ver las reuniones de sus propios órganos
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@35382 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/resources/database/goc_reuniones_0.1.5.sql
+++ goc-base/src/main/resources/database/goc_reuniones_0.1.5.sql
@@ -16,3 +16,37 @@ CREATE INDEX UJI_REUNIONES.UJI_reuniones_invitados_reu_i
   (REUNION_ID);
 
 
+CREATE OR REPLACE FORCE VIEW UJI_REUNIONES.GOC_VW_REUNIONES_EDITORES
+(
+    ID,
+    ASUNTO,
+    ASUNTO_ALT,
+    FECHA,
+    DURACION,
+    NUM_DOCUMENTOS,
+    EDITOR_ID,
+    COMPLETADA,
+    EXTERNO,
+    ORGANO_ID,
+    TIPO_ORGANO_ID
+)
+AS
+  SELECT
+    r.id,
+    r.asunto,
+    r.asunto_alt,
+    r.fecha,
+    r.duracion,
+    (SELECT COUNT(*)
+     FROM goc_reuniones_documentos rd
+     WHERE rd.reunion_id = r.id)
+                 num_documentos,
+    r.creador_id editor_id,
+    r.completada completada,
+    o.externo,
+    o.organo_id,
+    o.tipo_organo_id
+  FROM goc_reuniones r,
+    goc_organos_reuniones o,
+    goc_organos_reuniones_miembros orm
+  WHERE r.id = o.reunion_id (+) AND o.id = orm.organo_reunion_id (+);
