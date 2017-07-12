2cf73f172aa5499a83aafa71658c4d196484dd74
Francisco Meliá <fmelia@gmail.com>
2017-07-12 13:18:25 +0000

    GOC-382 Revisar botones de acción para que no salgan en gris sino en azul
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@35741 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-base/src/main/webapp/app/view/reunion/FormReunionMiembros.js
+++ goc-base/src/main/webapp/app/view/reunion/FormReunionMiembros.js
@@ -1,129 +1,135 @@
 Ext.define('goc.view.reunion.FormReunionMiembros', {
-    extend: 'Ext.window.Window',
-    xtype: 'formReunionMiembros',
-    width: 640,
-    modal: true,
-    bodyPadding: 10,
-    layout: {
-        type: 'vbox',
+    extend : 'Ext.window.Window',
+    xtype : 'formReunionMiembros',
+    width : 640,
+    modal : true,
+    bodyPadding : 10,
+    layout : {
+        type : 'vbox',
         align : 'stretch',
-        pack  : 'start'
+        pack : 'start'
     },
-    title: appI18N.reuniones.gestionAsistentes,
+    title : appI18N.reuniones.gestionAsistentes,
 
-    requires: [
+    requires : [
         'goc.view.reunion.FormReunionMiembrosController'
     ],
-    controller: 'formReunionMiembrosController',
-    viewModel: {
-        type: 'reunionViewModel'
+    controller : 'formReunionMiembrosController',
+    viewModel : {
+        type : 'reunionViewModel'
     },
-    tbar: [
+    tbar : [
         {
-            xtype: 'button',
-            iconCls: 'fa fa-plus',
-            text: appI18N.reuniones.anadirSuplente,
-            handler: 'onAddSuplente',
-            bind: {
-                disabled: '{!admiteSuplencia}'
+            xtype : 'button',
+            iconCls : 'fa fa-plus',
+            text : appI18N.reuniones.anadirSuplente,
+            handler : 'onAddSuplente',
+            bind : {
+                disabled : '{!admiteSuplencia}'
             }
         },
         {
-            xtype: 'button',
-            iconCls: 'fa fa-edit',
-            text: appI18N.reuniones.borrarSuplente,
-            handler: 'onRemoveSuplente',
-            bind: {
-                disabled: '{!admiteSuplencia}'
+            xtype : 'button',
+            iconCls : 'fa fa-edit',
+            text : appI18N.reuniones.borrarSuplente,
+            handler : 'onRemoveSuplente',
+            bind : {
+                disabled : '{!admiteSuplencia}'
             }
         }
     ],
 
-    items: [
+    items : [
         {
-            xtype: 'textfield',
-            emptyText: appI18N.reuniones.buscaAsistente,
-            name: 'searchMiembro',
-            listeners: {
-                change: 'onSearchMiembro'
+            xtype : 'textfield',
+            emptyText : appI18N.reuniones.buscaAsistente,
+            name : 'searchMiembro',
+            listeners : {
+                change : 'onSearchMiembro'
             }
         },
         {
-            xtype: 'grid',
-            flex: 1,
+            xtype : 'grid',
+            flex : 1,
             height : 300,
-            scrollable: true,
-            margin: '5 0 5 0',
-            viewConfig: {
-                emptyText: appI18N.reuniones.sinAsistentes,
-                markDirty: false
+            scrollable : true,
+            margin : '5 0 5 0',
+            viewConfig : {
+                emptyText : appI18N.reuniones.sinAsistentes,
+                markDirty : false
             },
-            bind: {
-                store: '{store}'
+            bind : {
+                store : '{store}'
             },
-            columns: [
+            columns : [
                 {
-                    text: appI18N.common.nombre,
-                    dataIndex: 'nombre',
-                    flex: 2
+                    text : appI18N.common.nombre,
+                    dataIndex : 'nombre',
+                    flex : 2
                 },
                 {
-                    text: appI18N.miembros.cargo,
-                    dataIndex: 'cargoNombre',
-                    flex: 1
+                    text : appI18N.miembros.cargo,
+                    dataIndex : 'cargoNombre',
+                    flex : 1
                 },
                 {
-                    text: appI18N.reuniones.correo,
-                    dataIndex: 'email',
-                    flex: 1
+                    text : appI18N.reuniones.correo,
+                    dataIndex : 'email',
+                    flex : 1
                 },
                 {
-                    dataIndex: 'suplenteId',
-                    hidden: true
+                    dataIndex : 'suplenteId',
+                    hidden : true
                 },
                 {
-                    text: appI18N.reuniones.suplente,
-                    dataIndex: 'suplenteNombre',
-                    flex: 1
+                    text : appI18N.reuniones.suplente,
+                    dataIndex : 'suplenteNombre',
+                    flex : 1
                 },
                 {
-                    text: appI18N.reuniones.suplenteEmail,
-                    dataIndex: 'suplenteEmail',
-                    flex: 1
+                    text : appI18N.reuniones.suplenteEmail,
+                    dataIndex : 'suplenteEmail',
+                    flex : 1
                 },
                 {
-                    text: appI18N.reuniones.asistencia,
-                    xtype: 'checkcolumn',
-                    dataIndex: 'asistencia',
-                    bind: {
-                        disabled: '{reunionCompletada}'
+                    text : appI18N.reuniones.asistencia,
+                    xtype : 'checkcolumn',
+                    dataIndex : 'asistencia',
+                    bind : {
+                        disabled : '{reunionCompletada}'
                     }
-                }]
+                }
+            ]
         }
     ],
 
-    bbar: [
-        '->', {
-            xtype: 'button',
-            text: appI18N.common.aceptar,
-            handler: 'onClose',
-            bind: {
-                disabled: '{reunionCompletada}'
-            }
-        }, {
-            xtype: 'panel',
-            html: '<a style="text-decoration: none; color: #222;" href="#">' + appI18N.common.cancelar + '</a>',
-            listeners: {
-                render: function (component) {
-                    component.getEl().on('click', 'onClose');
+    bbar : {
+        defaultButtonUI : 'default',
+        items : [
+            '->', {
+                xtype : 'button',
+                text : appI18N.common.aceptar,
+                handler : 'onClose',
+                bind : {
+                    disabled : '{reunionCompletada}'
+                }
+            }, {
+                xtype : 'panel',
+                html : '<a style="text-decoration: none; color: #222;" href="#">' + appI18N.common.cancelar + '</a>',
+                listeners : {
+                    render : function(component)
+                    {
+                        component.getEl().on('click', 'onClose');
+                    }
                 }
             }
-        }],
+        ]
+    },
 
-    listeners: {
-        show: 'onLoad',
-        onSave: 'onSave',
-        onAddSuplente: 'onAddSuplente',
-        onRemoveSuplente: 'onRemoveSuplente'
+    listeners : {
+        show : 'onLoad',
+        onSave : 'onSave',
+        onAddSuplente : 'onAddSuplente',
+        onRemoveSuplente : 'onRemoveSuplente'
     }
 });
