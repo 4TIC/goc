13d3c3b9e6d50c3b938a3958262675b2e50007a5
sergiogragera <sergiogragera@gmail.com>
2017-11-20 10:43:47 +0100

    Añadida url del servicio de firma

--- goc-base/src/main/java/es/uji/apps/goc/firmas/FirmaService.java
+++ goc-base/src/main/java/es/uji/apps/goc/firmas/FirmaService.java
@@ -1,17 +1,22 @@
 package es.uji.apps.goc.firmas;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import es.uji.apps.goc.dto.ReunionFirma;
 import es.uji.apps.goc.model.RespuestaFirma;
-import org.springframework.stereotype.Component;
 
 @Component
 public class FirmaService
 {
+    @Value("${goc.publicUrl}")
+    private String publicUrl;
+
     public RespuestaFirma firmaReunion(ReunionFirma reunionFirma)
     {
         RespuestaFirma respuestaFirma = new RespuestaFirma();
 
-        respuestaFirma.setUrlActa("http://www.uji.es/acta");
+        respuestaFirma.setUrlActa(publicUrl + "/goc/rest/actas/" + reunionFirma.getId());
 
         return respuestaFirma;
     }
