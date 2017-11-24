95e16a727296756a548a1f6ee367806cf20f6eb0
Francisco Meliá <fmelia@gmail.com>
2017-11-24 11:55:54 +0000

    GOC-444 Como convocado quiero recibir la información del orden del día en el correo de la convocatoria
    git-svn-id: http://jira.uji.es/repos/uji/GOC/goc/trunk@36969 e6edf6fb-f266-4316-afb4-e53d95876a76

--- goc-core/src/main/java/es/uji/apps/goc/notifications/ReunionFormatter.java
+++ goc-core/src/main/java/es/uji/apps/goc/notifications/ReunionFormatter.java
@@ -2,6 +2,7 @@ package es.uji.apps.goc.notifications;
 
 import java.text.SimpleDateFormat;
 
+import es.uji.apps.goc.dto.PuntoOrdenDia;
 import es.uji.apps.goc.dto.Reunion;
 
 public class ReunionFormatter
@@ -26,20 +27,52 @@ public class ReunionFormatter
             content.append("<div>" + textoAux + "</div><br/>");
         }
 
+        if (reunion.getNumeroSesion() != null)
+        {
+            content.append(
+                    "<div><strong>Número de la sessió: </strong><span>" + reunion.getNumeroSesion() + "</span></div>");
+        }
+
         if (reunion.getDescripcion() != null && !reunion.getDescripcion().isEmpty())
         {
             content.append("<div><strong>Descripció: </strong><span>" + reunion.getDescripcion() + "</span></div>");
         }
 
-        content.append("<div><strong>Data i hora: </strong>" + formatter.format(reunion.getFecha()) + "</div>");
-
-        if (reunion.getFechaSegundaConvocatoria() != null)
+        if (reunion.getFechaSegundaConvocatoria() == null)
         {
+            content.append("<div><strong>Data i hora: </strong>" + formatter.format(reunion.getFecha()) + "</div>");
+        }
+        else
+        {
+            content.append(
+                    "<div><strong>Primera convocatòria: </strong>" + formatter.format(reunion.getFecha()) + "</div>");
             content.append("<div><strong>Segona convocatòria: </strong>" + formatter.format(
                     reunion.getFechaSegundaConvocatoria()) + "</div>");
         }
 
-        content.append("<div><strong>Duració: </strong>" + reunion.getDuracion() + " minuts</div><br/>");
+        if (reunion.getUbicacion() != null && !reunion.getUbicacion().isEmpty())
+        {
+            content.append("<div><strong>Ubicació: </strong>" + reunion.getUbicacion() + "</div>");
+        }
+
+        if (reunion.getDuracion() != null && reunion.getDuracion() > 0)
+        {
+            content.append("<div><strong>Duració: </strong>" + reunion.getDuracion() + " minuts</div>");
+        }
+
+        if (reunion.getReunionPuntosOrdenDia() != null && !reunion.getReunionPuntosOrdenDia().isEmpty())
+        {
+            content.append("<h4>Ordre del dia</h4>");
+            content.append("<ol>");
+
+            for (PuntoOrdenDia puntoOrdenDia : reunion.getReunionPuntosOrdenDia())
+            {
+                content.append("<li>" + puntoOrdenDia.getTitulo() + "</li>");
+            }
+
+            content.append("</ol>");
+        }
+
         content.append(
                 "<div>Per a més informació, podeu consultar el detall de la reunió a <a href=\"" + publicUrl + "/goc/rest/publicacion/reuniones/" + reunion
                         .getId() + "\">" + publicUrl + "/goc/rest/publicacion/reuniones/" + reunion.getId() + "</div>");
